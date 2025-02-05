a9e6dde351aa6e6447866d16ac14940dfec96ffe
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2025-02-05 16:03:27 +0000

    added HelloWorl.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// says Hello world and more
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Alex,");
+        System.out.println("you suck!");
+    }
+}
